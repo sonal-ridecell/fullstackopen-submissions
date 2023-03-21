4c5201e5e41740ea7e1857d78f21d3439bab85e2
Sonal Agrawal <sonal.agrawal@ridecell.com>
2023-03-21 17:01:47 +0530

    part 1b refactor

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,28 +1,30 @@
 const Header = (props) => {
+  console.log("Header", props)
   return (
     <h1>{props.course}</h1>
   )
 }
 
 const Part = (props) => {
+  console.log("Part", props)
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
 
 const Content = (props) => {
+  console.log("Content", props)
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
+      {props.parts.map(part => <Part part={part} />)}
     </div>
   )
 }
 
 const Total = (props) => {
+  console.log("Total", props)
   return (
     <p>
       Number of exercises {props.total}
@@ -31,19 +33,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1= {exercises1} part2={part2} exercises2= {exercises2} part3={part3} exercises3= {exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts.reduce((acc, part) => acc + part.exercises, 0)} />
     </div>
   )
 }
